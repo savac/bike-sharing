502d08a082ee8589b38cb9f3878b9f455e027beb
QuentinY <quentin.yoshida@outlook.com>
2015-03-29 23:43:04 +0200

    Sub5 ~0.46

---
+++ code/KaggleC4GBM.R
@@ -0,0 +1,77 @@
+library(xts)
+library(gbm)
+
+Sys.setlocale("LC_ALL","English")
+
+train <- read.csv("../train.csv")
+test <- read.csv("../test.csv")
+
+## Train
+#Convert datetime into timestamps (split day and hour)
+train$datetime <- as.character(train$datetime)
+train$datetime <- strptime(train$datetime, format="%Y-%m-%d %T", tz="EST")
+ind_train <- unique(format(train$datetime, format = "%Y-%m"))
+
+#convert hours to factors in separate feature
+train$hour <- as.integer(substr(train$datetime, 12,13))
+train$hour <- as.factor(train$hour)
+
+#Day of the week
+train$weekday <- as.factor(weekdays(train$datetime))
+
+train$datetime <- strptime(train$datetime, format="%Y-%m-%d")
+
+train_xts <- xts(train[,-1], order.by = train$datetime)
+
+## Test
+#Convert datetime into timestamps (split day and hour)
+test$datetime <- as.character(test$datetime)
+test$datetime <- strptime(test$datetime, format="%Y-%m-%d %T", tz="EST")
+
+#convert hours to factors in separate feature
+test$hour <- as.integer(substr(test$datetime, 12,13))
+test$hour <- as.factor(test$hour)
+
+#Day of the week
+test$weekday <- as.factor(weekdays(test$datetime))
+
+test$datetime <- strptime(test$datetime, format="%Y-%m-%d")
+
+test_xts <- xts(test[,-1], order.by = test$datetime)
+
+res2 <- NULL
+
+for (ind in ind_train) {
+  print(ind)
+  
+  # Growing window
+  tmp <- as.data.frame(train_xts[paste0("/", ind)])
+  tmp_test <- as.data.frame(test_xts[ind])
+  row.names(tmp) <- NULL
+  row.names(tmp_test) <- NULL
+  
+  y_casual <- as.numeric(tmp[,"casual"])
+  y_reg <- as.numeric(tmp[,"registered"])
+  tmp <- tmp[, !names(tmp) %in% c("casual", "registered", "count")]
+  
+  names <- c("season", "holiday", "workingday", "weather", "hour", "weekday") # Factors
+  names_num <- c("temp", "atemp", "humidity", "windspeed") # Numerics
+  
+  dataset <- rbind(cbind(tmp, typ = 0), cbind(tmp_test, typ = 1))
+  dataset[, names_num] <- lapply(dataset[,names_num], as.numeric)
+  dataset[, names] <- lapply(dataset[,names], as.factor)
+  
+  casual <- gbm.fit(dataset[dataset$typ == 0, -11], y = log(y_casual + 1), distribution ="gaussian", shrinkage=0.1, 
+                    n.trees = 100, interaction.depth = 6, verbose = F)
+  registered <- gbm.fit(dataset[dataset$typ == 0, -11], y = log(y_reg + 1), distribution ="gaussian", shrinkage=0.1, 
+                        n.trees = 100, interaction.depth = 6, verbose = F)
+  pred_casual <- predict(casual, dataset[dataset$typ == 1, -11], n.trees = 100)
+  pred_reg <- predict(registered, dataset[dataset$typ == 1, -11], n.trees = 100)
+
+  res2 <- rbind(res2, as.matrix(round(exp(pred_casual) + exp(pred_reg) - 2, 0)))
+  
+}
+
+sub <- read.csv("../sampleSubmission.csv")
+sub$count <- res2
+write.csv(sub, file = "../sub5.csv", row.names = F, quote = F)
